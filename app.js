0a38ce671ea2800176e200806e4a072d78c1aa8b
Tonghong Chen <tonghong.july@gmail.com>
2020-04-18 21:26:33 -0700

    Added a readme file

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const reviewRouter = require('./routes/reviewRoutes')
 const bookingRouter = require('./routes/bookingRoutes')
 const viewRouter = require('./routes/viewRoutes')
 
+// Start express app
 const app = express()
 
 // Express automatically support all the most common template engine, such as Pug
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Natours Application
+
+Fully-functional tour booking application from Udemy 42hr Nodejs Bootcamp
+https://www.udemy.com/course/nodejs-express-mongodb-bootcamp
